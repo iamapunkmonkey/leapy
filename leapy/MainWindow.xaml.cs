8fb96f354d5b0352a05a4465dbb3b8dc524ff4e0
Zach Breman <zachary.breman@ticketmaster.com>
2020-10-14 13:01:44 -0500

    Adding image display

--- leapy/MainWindow.xaml.cs
+++ leapy/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,10 @@ namespace leapy
 		public MainWindow ()
 		{
 			InitializeComponent();
+
 		}
+//			Thread.Sleep( 10000 );
+//		System.Windows.Application.Current.Shutdown();
+
 	}
 }
